e5659cd9e6c2d526f6b1b4baf6dae03742b21550
xTheSnopx <xjuanesh@gmail.com>
2025-03-28 08:52:49 -0500

    con put

--- Back-end/crud_basic/src/main/java/com/sena/crud_basic/service/OrdersService.java
+++ Back-end/crud_basic/src/main/java/com/sena/crud_basic/service/OrdersService.java
@@ -1,5 +1,8 @@
 package com.sena.crud_basic.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -9,9 +12,6 @@ import com.sena.crud_basic.DTO.responseDTO;
 import com.sena.crud_basic.model.Orders;
 import com.sena.crud_basic.repository.IOrders;
 
-import java.util.List;
-import java.util.Optional;
-
 @Service
 public class OrdersService {
     @Autowired
@@ -20,9 +20,11 @@ public class OrdersService {
     public List<Orders> finAll(){
         return data.findAll();
     }
+
     public Optional<Orders> findById(int orderID){
         return data.findById(orderID);
     }
+
     public responseDTO deleteOrders(int orderId){
         if(!findById(orderId).isPresent()){
             responseDTO respuesta = new responseDTO(
@@ -35,28 +37,31 @@ public class OrdersService {
         responseDTO respuesta = new responseDTO(
             HttpStatus.OK.toString(),
             "La Orden fue eliminada correctamente"
-            );
+        );
         return respuesta;
     }
+
     public responseDTO save(OrdersDTO OrdersDTO){
         Orders Orders = converToModel(OrdersDTO);
         data.save(Orders);
         responseDTO respuesta = new responseDTO(
             HttpStatus.OK.toString(), 
             "Se guardo correctamente"
-            );
+        );
         return respuesta;
     }
+
     public OrdersDTO converToDTO(Orders Orders){
-        OrdersDTO OrdersDTO = new OrdersDTO
-        (0, 
-        Orders.getUser(), 
-        Orders.getOrderDate(), 
-        Orders.getTotal(), 
-        Orders.getStatus()
+        OrdersDTO OrdersDTO = new OrdersDTO(
+            0, 
+            Orders.getUser(), 
+            Orders.getOrderDate(), 
+            Orders.getTotal(), 
+            Orders.getStatus()
         );
         return OrdersDTO;
     }
+
     public Orders converToModel(OrdersDTO OrdenDTO){
         Orders Orders = new Orders(
             0, 
@@ -64,7 +69,39 @@ public class OrdersService {
             OrdenDTO.getOrderDate(),
             OrdenDTO.getTotal(),
             OrdenDTO.getStatus()
-            );
+        );
         return Orders;
+    }
+
+    // Método para actualizar una orden
+    public responseDTO update(int orderId, OrdersDTO OrdersDTO){
+        // Buscar la orden existente
+        Optional<Orders> existingOrderOpt = data.findById(orderId);
+
+        if (existingOrderOpt.isPresent()) {
+            // Si la orden existe, obtenemos el objeto y actualizamos sus valores
+            Orders existingOrder = existingOrderOpt.get();
+            existingOrder.setUser(OrdersDTO.getUser());
+            existingOrder.setOrderDate(OrdersDTO.getOrderDate());
+            existingOrder.setTotal(OrdersDTO.getTotal());
+            existingOrder.setStatus(OrdersDTO.getStatus());
+
+            // Guardamos la orden actualizada
+            data.save(existingOrder);
+
+            // Retornamos una respuesta con el estado de la actualización
+            responseDTO respuesta = new responseDTO(
+                HttpStatus.OK.toString(),
+                "La orden fue actualizada correctamente"
+            );
+            return respuesta;
+        } else {
+            // Si no se encuentra la orden, retornamos un error
+            responseDTO respuesta = new responseDTO(
+                HttpStatus.NOT_FOUND.toString(),
+                "La orden no existe"
+            );
+            return respuesta;
+        }
     }
 }
